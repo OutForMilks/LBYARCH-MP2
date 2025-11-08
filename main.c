462876ad1b2a70fbdfb4fa01096e8957269d37ff
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-11-08 19:26:47 +0800

    float -> double since reqs require double-precision

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() { // take in inputs here
     int rows, columns; // size of matrix
     scanf("%d %d", &rows, &columns); // user input (first line)
 
-    float matrix[rows][columns]; // store the image pixel data
+    double matrix[rows][columns]; // store the image pixel data
 
     return 0;
 }
