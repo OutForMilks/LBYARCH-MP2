eeae30fe2c06b76d5ccaeff32e410e96c1516e13
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-11-08 19:26:08 +0800

    first user input line added. some declarations of variables added.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() { // take in inputs here
+    int rows, columns; // size of matrix
+    scanf("%d %d", &rows, &columns); // user input (first line)
+
+    float matrix[rows][columns]; // store the image pixel data
+
+    return 0;
+}
