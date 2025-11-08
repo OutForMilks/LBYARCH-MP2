a71ccd19d1292f7ae01cca17ea7d700c163d6af9
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-11-08 19:30:15 +0800

    added for loop for 2d

--- main.c
+++ main.c
@@ -4,7 +4,13 @@ int main() { // take in inputs here
     int rows, columns; // size of matrix
     scanf("%d %d", &rows, &columns); // user input (first line)
 
-    double matrix[rows][columns]; // store the image pixel data
+    double imgData[rows][columns]; // store the image pixel data
+
+    for (int i = 0 ; i < rows ; i++) { // loop through rows
+        for (int j = 0 ; j < columns ; j++) { // loop through columns per row
+            scanf("%lf", imgData[i][j]); // put the input in its respective spot in the array
+        }
+    }
 
     return 0;
 }
