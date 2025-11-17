cce9b82604fb0f9eb1a894e999ff17e8ed7785de
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-11-17 13:48:14 +0800

    Update imgCvtGrayDoubleToInt.asm

--- imgCvtGrayDoubleToInt.asm
+++ imgCvtGrayDoubleToInt.asm
@@ -3,12 +3,15 @@ global imgCvtGrayDoubleToInt ; C can call this
 
 imgCvtGrayDoubleToInt:
 
+	CVTSI2SD XMM15, R9
 loop:
     MOVSD XMM1, [RCX] ; copy double (current index)
-    MOVSD [RDX], XMM1 ; paste double (current index)
+    MULSD XMM1, XMM15
+    CVTSD2SI R11, XMM1
+    MOV [RDX], R11D ; paste double (current index)
     
     ADD RCX, 8 ; move to next index
-    ADD RDX, 8 ; move to next index
+    ADD RDX, 4 ; move to next index
 
     DEC R8 ; decrement counter  
     JNZ loop ; restart until 0
