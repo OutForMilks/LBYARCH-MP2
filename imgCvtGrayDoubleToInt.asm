0409e6da6cf92b0ecdeb0021899703232718d8e0
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-11-22 23:40:16 +0800

    feat: the assembly file does not need a 255 input

--- imgCvtGrayDoubleToInt.asm
+++ imgCvtGrayDoubleToInt.asm
@@ -1,12 +1,13 @@
+section .data
+    val dq 255.0
 section .text
 global imgCvtGrayDoubleToInt ; C can call this
 
 imgCvtGrayDoubleToInt:
 
-	CVTSI2SD XMM15, R9
 loop:
-    MOVSD XMM1, [RCX] ; copy double (current index)
-    MULSD XMM1, XMM15
+    MOVSD XMM1, qword [RCX] ; copy double (current index)
+    MULSD XMM1, qword [rel val]; rel is used to get the value relative to the instruction (inside this )
     CVTSD2SI R11, XMM1
     MOV [RDX], R11D ; paste double (current index)
     
